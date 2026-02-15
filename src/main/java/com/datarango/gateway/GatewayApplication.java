c257568d3aaccba540f8141809b3e599751b3fba
Samson Okunola <smsnmicheal@gmail.com>
2026-02-15 22:05:06 +0100

    feat: enhance API Gateway with service discovery and health checks

--- src/main/java/com/datarango/gateway/GatewayApplication.java
+++ src/main/java/com/datarango/gateway/GatewayApplication.java
@@ -3,8 +3,10 @@ package com.datarango.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
--- src/main/java/com/datarango/gateway/config/RestClientConfig.java
+++ src/main/java/com/datarango/gateway/config/RestClientConfig.java
@@ -1,6 +1,7 @@
 package com.datarango.gateway.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestClientConfig {
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(5000);
